cacc1766e6fa9ab9122c6f46f22b73c530b0b70b
Pete Sevander <pete.sevander@gmail.com>
2024-01-26 19:54:28 +0200

    gitignore and splash scene

---
+++ splash.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://main.tscn")
