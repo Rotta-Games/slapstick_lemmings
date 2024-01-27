ec72325e542f30948839c559892f892171a9af1f
Joni Louhela <louhela.joni@gmail.com>
2024-01-27 20:07:44 +0200

    Fix cannon shoot direction

--- scenes/props/cannon.gd
+++ scenes/props/cannon.gd
@@ -28,8 +28,9 @@ func _fire():
 	var cb = cannonball_scene.instantiate()
 	get_tree().root.add_child(cb)
 	cb.position = cannonball_spawn_point.global_position
-	cb.apply_impulse(Vector2.from_angle(raycast.rotation + PI / 2) * FIRE_POWER)
-	self.apply_impulse(Vector2.from_angle(raycast.rotation + PI / 2) * 100 * -1)
+	print(raycast.rotation)
+	cb.apply_impulse(Vector2.from_angle(self.rotation + raycast.rotation + PI / 2) * FIRE_POWER)
+	self.apply_impulse(Vector2.from_angle(self.rotation + raycast.rotation + PI / 2) * 100 * -1)
 	animated_sprited.play("idle")
 	fire_delay = FIRE_DELAY_MAX
 	fire_sound.play()
