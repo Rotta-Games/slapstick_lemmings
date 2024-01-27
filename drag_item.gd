1e4d99bc37851cc21cd3d245d419caee90aa944e
Pete Sevander <pete.sevander@gmail.com>
2024-01-27 20:51:48 +0200

    Drag them all

--- drag_item.gd
+++ drag_item.gd
@@ -2,6 +2,8 @@ extends TextureRect
 
 @export var item_scene: PackedScene
 
+var item_body: PackedScene = preload("res://scenes/item_body.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +19,8 @@ func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			Global.is_dragging = true
-			var container = get_node("%DragContainer")
-			var item = item_scene.instantiate()
-			container.add_child(item)
+
+			var item = item_body.instantiate()
+			item.prop_scene = item_scene
+			get_tree().root.add_child(item)
 
--- scenes/props/banana.gd
+++ scenes/props/banana.gd
@@ -10,39 +10,6 @@ var last_mouse_pos = Vector2()
 var last_diff = float()
 var is_destroyed = true
 
-func _ready():
-	var mouse_pos = get_viewport().get_mouse_position()
-	set_global_position(mouse_pos)
-	self.gravity_scale = 0
-
-
-func _process(delta):
-	if draggable and Global.is_dragging:
-		var mouse_pos = get_viewport().get_mouse_position()
-
-		var pos = get_global_position()
-		last_mouse_pos = mouse_pos
-
-		var diff = pos.distance_to(mouse_pos)
-		last_diff = diff
-
-		set_global_position(mouse_pos)
-
-
-
-func _input(event):
-	if not draggable:
-		return
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-			self.draggable = false
-			self.gravity_scale = 1
-			var mouse_pos = get_viewport().get_mouse_position()
-
-			var diff = mouse_pos - last_mouse_pos
-			apply_central_force(diff * 1000)
-
-
 func _on_static_body_2d_body_entered(body):
 	if is_destroyed:
 		pass
