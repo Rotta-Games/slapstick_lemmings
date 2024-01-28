c84fefe721e80f30669c9a13f56de04ba2541c81
Pete Sevander <pete.sevander@gmail.com>
2024-01-28 13:53:31 +0200

    endtimer fixes

--- LaughMeter.gd
+++ LaughMeter.gd
@@ -3,12 +3,9 @@ extends Node2D
 @onready var time_label = $TimeLabel
 @onready var timer: Timer = get_tree().get_root().get_node("Node2D/EndTimer") 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var format_str = "Time remaining\n%ss"
-	time_label.text = format_str % str(int(timer.time_left))
+	var time = timer.wait_time if timer.paused else timer.time_left
+
+	time_label.text = format_str % str(int(time))
 	
--- scripts/level_handler.gd
+++ scripts/level_handler.gd
@@ -4,6 +4,8 @@ enum LevelState {IDLE, ACTIVE}
 var current_state = LevelState.IDLE
 signal level_state_changed(new_state)
 
+@onready var end_timer = $EndTimer
+
 func _ready():
 	$FilmButtons/Control/ActionButton.pressed.connect(_toggle_level_state)
 
@@ -11,9 +13,11 @@ func _toggle_level_state():
 	if current_state == LevelState.IDLE:
 		level_state_changed.emit(LevelState.ACTIVE)
 		current_state = LevelState.ACTIVE
+		end_timer.start()
 	else:
 		level_state_changed.emit(LevelState.IDLE)
 		current_state = LevelState.IDLE
+		end_timer.stop()
 		reset_level()
 		
 func reset_level():
