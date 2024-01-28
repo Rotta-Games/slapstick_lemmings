4f72360bc0eb7dde8cfec17b014e8df6820be42b
Pete Sevander <pete.sevander@gmail.com>
2024-01-28 12:59:42 +0200

    fix bomb

--- scenes/props/bommi.gd
+++ scenes/props/bommi.gd
@@ -1,13 +1,18 @@
 extends RigidBody2D
 
+@onready var sprite = $AnimatedSprite2D
+@onready var shape = $CollisionShape2D
+
+var timer
 
 func _on_timer_timeout():
 	var tween = get_tree().create_tween()
-	tween.tween_property($CollisionShape2D.shape, "radius", 100, 0.1)
+	tween.tween_property(shape.shape, "radius", 100, 0.1)
 	tween.tween_callback(self.queue_free)
 
 
 func _input(event):
 	if event is InputEventMouseButton and not event.pressed:
-		$AnimatedSprite2D.play("boom")
-		$Timer.start()
+		sprite.play("boom")
+		timer = get_tree().create_timer(3.0)
+		timer.connect("timeout", self._on_timer_timeout)
