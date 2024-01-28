72fed7c251612de9926c303f37c38e3aefe7927a
Pete Sevander <pete.sevander@gmail.com>
2024-01-28 13:10:04 +0200

    bomb sound

--- scenes/props/bommi.gd
+++ scenes/props/bommi.gd
@@ -2,13 +2,16 @@ extends RigidBody2D
 
 @onready var sprite = $AnimatedSprite2D
 @onready var shape = $CollisionShape2D
+@onready var sound = $FireSound
 
 var timer
 
 func _on_timer_timeout():
 	var tween = get_tree().create_tween()
+	sound.play()
 	tween.tween_property(shape.shape, "radius", 100, 0.1)
-	tween.tween_callback(self.queue_free)
+	var kill_timer = get_tree().create_timer(0.5)
+	kill_timer.connect("timeout", self._on_fire_sound_finished)
 
 
 func _input(event):
@@ -16,3 +19,8 @@ func _input(event):
 		sprite.play("boom")
 		timer = get_tree().create_timer(3.0)
 		timer.connect("timeout", self._on_timer_timeout)
+
+
+func _on_fire_sound_finished():
+	queue_free()
+
