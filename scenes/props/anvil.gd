cdc5775c63c721b7f23560a2b45f1e3beb87f767
Pete Sevander <pete.sevander@gmail.com>
2024-01-28 15:03:31 +0200

    web export test

--- scenes/props/anvil.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	add_to_group("Items")
--- scripts/Global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var score = 0
-
-const SCORE_FOR_1_STAR = 2000
-const SCORE_FOR_2_STAR = 8000
-const SCORE_FOR_3_STAR = 20000
-
-var is_dragging = false
