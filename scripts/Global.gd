32ca5e030a439e1f491a89309fee0ed394419cf8
Joni Louhela <louhela.joni@gmail.com>
2024-01-28 14:47:38 +0200

    Adjust score values

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -2,8 +2,8 @@ extends Node
 
 var score = 0
 
-const SCORE_FOR_1_STAR = 3000
-const SCORE_FOR_2_STAR = 10000
+const SCORE_FOR_1_STAR = 2000
+const SCORE_FOR_2_STAR = 8000
 const SCORE_FOR_3_STAR = 20000
 
 var is_dragging = false
